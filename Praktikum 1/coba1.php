d62511acf88ad03f2fb940054d1ae9eb325e88b8
RizkyIlham <114557035+RizkyIlham@users.noreply.github.com>
2023-03-04 08:12:34 +0700

    Add files via upload

---
+++ Praktikum 1/coba1.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('PHI', 3.14);
+    define('DBNAME', 'Inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nama database nya :' . DBNAME;
+    echo '<br> Lokasi database nya :' . DBSERVER;
---
+++ Praktikum 1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    $nama = 'Ilham';
+    $umur = 19;
+    $berat = 50;
+
+    echo 'Nama Saya :' . $nama;
+    echo '<br> Umur Saya :' . $umur;
+    echo '<br> Berat Saya :' . $berat;
